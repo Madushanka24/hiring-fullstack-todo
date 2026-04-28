636d16beac710dc711e84b7cd1b32a7bdfd3b19a
Madushanka Sandaruwan <142202054+Madushanka24@users.noreply.github.com>
2026-04-28 14:55:12 +0530

    Delete server directory

--- server/models/Todo.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const todoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  done: { type: Boolean, default: false }
-}, { timestamps: true });
-
-export default mongoose.model("Todo", todoSchema);
