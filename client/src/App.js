fac6435ce93d168bff69b4f5e864fe3ea7dfd8f1
Dillow <madushanka20021224@gmail.com>
2026-04-28 15:21:32 +0530

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import API from "./api";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const fetchTodos = async () => {
+    const res = await API.get("/todos");
+    setTodos(res.data);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const addTodo = async () => {
+    if (!title) return;
+
+    await API.post("/todos", { title });
+    setTitle("");
+    fetchTodos();
+  };
+
+  const toggleTodo = async (id) => {
+    await API.patch(`/todos/${id}/done`);
+    fetchTodos();
+  };
+
+  const deleteTodo = async (id) => {
+    await API.delete(`/todos/${id}`);
+    fetchTodos();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>TODO App</h1>
+
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="New todo"
+      />
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            <span
+              onClick={() => toggleTodo(todo._id)}
+              style={{
+                textDecoration: todo.done ? "line-through" : "none",
+                cursor: "pointer"
+              }}
+            >
+              {todo.title}
+            </span>
+
+            <button onClick={() => deleteTodo(todo._id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
