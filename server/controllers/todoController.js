a8b0331118794d213a5173706bc97f382baa8b79
Dillow <madushanka20021224@gmail.com>
2026-04-28 13:11:57 +0530

    create done

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -6,3 +6,13 @@ export const getTodos = async (req, res) => {
   const todos = await Todo.find().sort({ createdAt: -1 });
   res.json(todos);
 };
+
+// CREATE
+export const createTodo = async (req, res) => {
+  const { title, description } = req.body;
+
+  const todo = new Todo({ title, description });
+  await todo.save();
+
+  res.status(201).json(todo);
+};
